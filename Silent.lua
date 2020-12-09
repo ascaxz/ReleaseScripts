4856ed396b4d97dda650c9e0b4df65f3dd2bc663
VorTex <65015743+ascaxz@users.noreply.github.com>
2020-12-09 20:42:29 +0700

    Update Silent.lua

--- Silent.lua
+++ Silent.lua
@@ -1,4 +1,70 @@
+local Players = game.Players
+local LocalPlayer = Players.LocalPlayer
+local ReplicatedFirst, ReplicatedStorage = game.ReplicatedFirst, game.ReplicatedStorage
 local CurrentCamera = workspace.CurrentCamera
+function NotObstructing(Destination, Ignore)
+    local Origin = CurrentCamera.CFrame.Position
+    local CheckRay = Ray.new(Origin, Destination - Origin)
+    local Hit = workspace.FindPartOnRayWithIgnoreList(workspace, CheckRay, Ignore)
+    return Hit == nil
+end
+function GetClosestToCursor()
+    local Max, Close = math.huge
+    for I,V in pairs(Players.GetPlayers(Players)) do
+        if V ~= LocalPlayer and V.Character and V.Character.Humanoid then
+            if Toggles.WallCheck then
+                if Toggles.TeamCheck then
+                    if V.Team ~= LocalPlayer.Team then
+                        local Body = (not BodyPart and "Head" or BodyPart)
+                        local Pos, Vis = CurrentCamera.WorldToScreenPoint(CurrentCamera, V.Character[Body].Position)
+                        if Vis and NotObstructing(V.Character[Body].Position, {LocalPlayer.Character, V.Character}) then
+                            local MyPos, TheirPos = Vector2.new(Mouse.X, Mouse.Y), Vector2.new(Pos.X, Pos.Y)
+                            if (TheirPos - MyPos).Magnitude < Max and (TheirPos - MyPos).Magnitude < Storage.FOV then
+                                Max = (TheirPos - MyPos).Magnitude
+                                Close = V
+                            end
+                        end
+                    end
+                else
+                    local Body = (not BodyPart and "Head" or BodyPart)
+                    local Pos, Vis = CurrentCamera.WorldToScreenPoint(CurrentCamera, V.Character[Body].Position)
+                    if Vis and NotObstructing(V.Character[Body].Position, {LocalPlayer.Character, V.Character}) then
+                        local MyPos, TheirPos = Vector2.new(Mouse.X, Mouse.Y), Vector2.new(Pos.X, Pos.Y)
+                        if (TheirPos - MyPos).Magnitude < Max and (TheirPos - MyPos).Magnitude < Storage.FOV then
+                            Max = (TheirPos - MyPos).Magnitude
+                            Close = V
+                        end
+                    end
+                end
+            else
+                if Toggles.TeamCheck then
+                    if V.Team ~= LocalPlayer.Team then
+                        local Body = (not BodyPart and "Head" or BodyPart)
+                        local Pos, Vis = CurrentCamera.WorldToScreenPoint(CurrentCamera, V.Character[Body].Position)
+                        if Vis then
+                            local MyPos, TheirPos = Vector2.new(Mouse.X, Mouse.Y), Vector2.new(Pos.X, Pos.Y)
+                            if (TheirPos - MyPos).Magnitude < Max and (TheirPos - MyPos).Magnitude < Storage.FOV then
+                                Max = (TheirPos - MyPos).Magnitude
+                                Close = V
+                            end
+                        end
+                    end
+                else
+                    local Body = (not BodyPart and "Head" or BodyPart)
+                    local Pos, Vis = CurrentCamera.WorldToScreenPoint(CurrentCamera, V.Character[Body].Position)
+                    if Vis then
+                        local MyPos, TheirPos = Vector2.new(Mouse.X, Mouse.Y), Vector2.new(Pos.X, Pos.Y)
+                        if (TheirPos - MyPos).Magnitude < Max and (TheirPos - MyPos).Magnitude < Storage.FOV then
+                            Max = (TheirPos - MyPos).Magnitude
+                            Close = V
+                        end
+                    end
+                end
+            end
+        end
+    end
+    return Close
+end
 local MT = getrawmetatable(game)
 local OldNC = MT.__namecall
 local OldIDX = MT.__index
