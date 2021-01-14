0cf5dc119b1692fe0e3e58d0c4c3f8adb0fe1a01
VorTex <65015743+ascaxz@users.noreply.github.com>
2021-01-14 20:58:43 +0700

    Update MTHookPF.lua

--- MTHookPF.lua
+++ MTHookPF.lua
@@ -1,5 +1,5 @@
 
-local A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,Old,T,U,V,W,X,Y,Z,A1 = ...
+local A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,Old,T,U,V,W,X,Y,Z = ...
 
 return function(self, K)
 	if A then
@@ -9,8 +9,8 @@ return function(self, K)
 			local Chosen = J == K and L[M:NextInteger(1, #L)] or J
 			local ChosenBodyPart = (Char and N(Char.rootpart.Parent, Chosen))
 			if ChosenBodyPart then
-				local _, TimeToHit = O(self[U], P, ChosenBodyPart[U], B[W][X][A1])
-				return T(self[U], ChosenBodyPart[U] + (R(unpack(S)) * (TimeToHit ^ 2)) + (ChosenBodyPart.Parent[Z][V] * TimeToHit))
+				local _, TimeToHit = O(self[T], P, ChosenBodyPart[T], B[V][W][Z])
+				return S(self[T], ChosenBodyPart[T] + (Q(unpack(R)) * (TimeToHit ^ 2)) + (ChosenBodyPart.Parent[Y][U] * TimeToHit))
 			end
 		end
 	end
