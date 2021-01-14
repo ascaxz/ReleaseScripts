b599e4130416bec122372f83e78eaa5d5649b36e
VorTex <65015743+ascaxz@users.noreply.github.com>
2021-01-14 21:04:02 +0700

    Update MTHookPF.lua

--- MTHookPF.lua
+++ MTHookPF.lua
@@ -1,11 +1,11 @@
 
-local A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,Old,T,U,V,W,X,Y,Z = ...
+local A,B,C,D,E,F,G,H,I,J,K1,L,M,N,O,P,Q,R,S,Old,T,U,V,W,X,Y,Z = ...
 return function(self, K)
 	if A then
 		if B[V] and B[V][W] and (self == B[V].barrel or C(self) == D) and K == E then
 			local Player = (F and G() or H())
 			local Char = (Player and I[Player])
-			local Chosen = J == K and L[M:NextInteger(1, #L)] or J
+			local Chosen = J == K1 and L[M:NextInteger(1, #L)] or J
 			local ChosenBodyPart = (Char and N(Char.rootpart.Parent, Chosen))
 			if ChosenBodyPart then
 				local _, TimeToHit = O(self[T], P, ChosenBodyPart[T], B[V][W][Z])
